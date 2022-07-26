6da9ddebadcd04a0150dae278353ba24a5c5f991
hnasser44 <hassan.nasser.2@st.ul.edu.lb>
2022-07-26 02:13:18 +0200

    updated mobile view

--- main.js
+++ main.js
@@ -2,17 +2,33 @@ const headerUL = document.getElementById('header-ul')
 const Article_header = document.querySelector('.hidden-header')
 
 
- function myFunction(x) {
+function myFunction(x) {
         if (x.matches) { // If media query matches
                 headerUL.classList.add('hide-ul')
                 Article_header.classList.remove('hidden-header')
         }
-        else{
+        else {
                 headerUL.classList.remove('hide-ul')
                 Article_header.classList.add('hidden-header')
         }
-      }
-      
+}
+
 var x = window.matchMedia("(min-width: 700px)")
 myFunction(x)
 x.addListener(myFunction)
+
+const hamburgerIcon = document.getElementById('hamburger')
+const closeIcon = document.getElementById('close-icon')
+const mobileMenu = document.querySelector('.mobile-menu')
+
+hamburgerIcon.addEventListener('click', () => {
+        hamburgerIcon.classList.add('hide')
+        closeIcon.classList.remove('hide')
+        mobileMenu.classList.remove('hide')
+})
+
+closeIcon.addEventListener('click', () => {
+        hamburgerIcon.classList.remove('hide')
+        closeIcon.classList.add('hide')
+        mobileMenu.classList.add('hide')
+})
